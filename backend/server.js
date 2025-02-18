31855794e583ec49dde7e5f7b76f44e42f3f8837
sudharson-c <sudharson@student.tce.edu>
2025-02-19 03:31:11 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,15 @@ const PORT = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors({
-    origin: "*"
-}));
+app.use(cors());
 app.use(express.json());
 
 // Routes
 app.use('/api', routes);
 app.get("/", (req, res) => res.send("welcome to user management api"));
-// Start the server
+// Start the server 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
-
-
+module.exports = app;
