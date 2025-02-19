95d4a3e983bdfca03989213eeb8dc113be50abd2
sudharson-c <sudharson@student.tce.edu>
2025-02-19 10:31:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
